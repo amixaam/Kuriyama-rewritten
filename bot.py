196e500fd22a93c4090deb2a2d487b5e4b023123
amixam <robertobrinko@gmail.com>
2022-06-30 16:53:02 +0300

    bot

---
+++ bot.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+import traceback
+import json
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = commands.Bot(command_prefix='k', intents=intents)
+
+with open(f"./json/settings.json", "r", encoding="utf-8") as f:
+    settings = json.load(f)
+
+@client.event
+async def on_ready():
+    await client.wait_until_ready()
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            client.load_extension(f'cogs.{filename[:-3]}')
+    print(f'Logged in as {client.user} || v{settings["version"]}')
+    
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        return
+    await ctx.send(error)
+    try:
+        raise error
+    except:
+        er = traceback.format_exc()
+        print(er)
+        c = await client.fetch_channel(settings["traceback"])
+        await c.send(f"``````py\n{er}\n``````")
+
+client.run('nande?')
---
+++ cogs/helpful.py
@@ -0,0 +1,41 @@
+import discord, datetime, os
+from discord.ext import commands
+
+class Helpful(commands.Cog):
+
+    def __init__(self, client):
+       self.client = client
+
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f'pingers: {round(self.client.latency * 1000)}ms')
+
+    #owner commands
+    @commands.command()
+    @commands.is_owner()
+    async def load(self, ctx, extension):
+        self.client.load_extension(f'cogs.{extension}')
+        await ctx.send('loaded!')
+
+    @commands.command()
+    @commands.is_owner()
+    async def unload(self, ctx, extension):
+        self.client.unload_extension(f'cogs.{extension}')
+        await ctx.send('unloaded!')
+
+    @commands.command()
+    @commands.is_owner()
+    async def reload(self, ctx, extension = None):
+        if (extension == None): #reload all cogs
+            for filename in os.listdir('./cogs'):
+                if filename.endswith('.py'):
+                    self.client.unload_extension(f'cogs.{filename[:-3]}')
+                    self.client.load_extension(f'cogs.{filename[:-3]}')
+            await ctx.send('reloaded!')
+        else: #reload specific cog
+            self.client.unload_extension(f'cogs.{extension}')
+            self.client.load_extension(f'cogs.{extension}')
+            await ctx.send('reloaded!')
+
+def setup(client):
+    client.add_cog(Helpful(client))
